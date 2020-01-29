a625bbebcae5de5faf89a04fbae3bf2b36c41fab
Enzo Maruffa <enzomm1999@gmail.com>
2020-01-28 21:12:08 -0300

    Adds camera feedback, adds vibrancy effect

--- Pagliacci/MenuViewController.swift
+++ Pagliacci/MenuViewController.swift
@@ -7,10 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MenuViewController: UIViewController {
 
     @IBOutlet weak var playContainer: UIView!
+    @IBOutlet weak var galleryContainer: UIView!
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +28,69 @@ class MenuViewController: UIViewController {
         playContainer.layer.shadowOpacity = 0.5
         playContainer.layer.shadowOffset = CGSize(width: 2, height: 2)
         playContainer.layer.shadowRadius = 2
-
+        
+        //
+        galleryContainer.layer.cornerRadius = 9
+        
+        galleryContainer.layer.shadowColor = UIColor.black.cgColor
+        galleryContainer.layer.shadowOpacity = 0.5
+        galleryContainer.layer.shadowOffset = CGSize(width: 2, height: 2)
+        galleryContainer.layer.shadowRadius = 2
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Setup your camera here...
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            
+            stillImageOutput = AVCapturePhotoOutput()
+
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+            
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+        
+    }
+    
+    func setupLivePreview() {
+        print("Previewing...")
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        //Step12
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+            self.captureSession.startRunning()
+            //Step 13
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
     }
     
 
